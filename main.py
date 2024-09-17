6c73eca64ffa8521648a76ef3bdb149be2bc0c8e
Radmir <iradmir1516@gmail.com>
2024-09-17 23:50:32 +0500

    GUI added

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import time
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import random
+import tkinter as tk
+from PIL import Image, ImageTk
 
 # Диапазон смещения курсора от центра снежинки вниз
-RAND_MIN = 5  # Мин пикселей
-RAND_MAX = 10  # Макс пикселей
+RAND_MIN = 5
+RAND_MAX = 10
 
 # Кнопка активации/деактивации кликера
 ACTIVE_BTN = keyboard.Key.ctrl_r
@@ -26,17 +28,21 @@ dark_green_lower = np.array([6, 22, 0])
 dark_green_upper = np.array([85, 255, 125])
 
 # Минимальная и максимальная площадь контура для фильтрации
-min_contour_area = 150  # Установите это значение в зависимости от ваших требований
+min_contour_area = 150
 max_contour_area = 1000
 
 clicking_enabled = False
 program_running = True
-executor = ThreadPoolExecutor(max_workers=10)  # Создаем пул потоков
+executor = ThreadPoolExecutor(max_workers=10)  # Пул потоков
 
 def on_press(key):
     global clicking_enabled, program_running
     try:
         if key == ACTIVE_BTN:
+            if not clicking_enabled:
+                button_status.config(text="ON", bg="green", fg="white", font=("Arial", 20))
+            else:
+                button_status.config(text="OFF", bg="red", fg="white", font=("Arial", 20))
             clicking_enabled = not clicking_enabled
             print(f"Clicking enabled: {clicking_enabled}")
         elif key == EXIT_BTN:
@@ -48,21 +54,16 @@ def on_press(key):
 
 def process_frame(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    # Создаем основную маску
-    mask = cv2.inRange(hsv, element_lower, element_upper)   
+    mask = cv2.inRange(hsv, element_lower, element_upper)
     
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Фильтруем контуры по площади и убираем вложенные контуры
     filtered_contours = []
     for i, cnt in enumerate(contours):
-        if min_contour_area <= cv2.contourArea(cnt) <= max_contour_area and hierarchy[0][i][3] == -1:  # Только верхние контуры
+        if min_contour_area <= cv2.contourArea(cnt) <= max_contour_area and hierarchy[0][i][3] == -1:
             filtered_contours.append(cnt)
     return filtered_contours
 
 def click_on_position(screen_x, screen_y):
-    global clicking_enabled
     if clicking_enabled:
         pyautogui.click(screen_x, screen_y + random.randint(RAND_MIN, RAND_MAX))
 
@@ -70,50 +71,104 @@ def click_element_contours(contours):
     global clicking_enabled
     for cnt in contours:
         if not clicking_enabled:
-            break  # Прекращаем обработку контуров, если клики отключены
+            break
         (x, y, w, h) = cv2.boundingRect(cnt)
         center_x = x + w // 2
         center_y = y + h // 2
         screen_x = region[0] + center_x
         screen_y = region[1] + center_y
-        executor.submit(click_on_position, screen_x, screen_y)  # Асинхронный клик
+        executor.submit(click_on_position, screen_x, screen_y)
 
 def capture_and_process():
     global program_running
     while program_running:
-        # Захват экрана
         screenshot = pyautogui.screenshot(region=region)
-        frame = np.array(screenshot)
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Преобразование в цветовое пространство BGR для OpenCV
+        frame_tk = np.array(screenshot)
+        frame = cv2.cvtColor(frame_tk, cv2.COLOR_RGB2BGR)
 
         contours = process_frame(frame)
 
         # Удаляем темно-зеленый цвет
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         dark_green_mask = cv2.inRange(hsv_frame, dark_green_lower, dark_green_upper)
-        frame[dark_green_mask > 0] = (0, 0, 0)  # Заменяем темно-зеленый цвет на черный
+        frame[dark_green_mask > 0] = (0, 0, 0)
 
-        # Рисуем контуры на кадре
         cv2.drawContours(frame, contours, -1, (0, 0, 255), 2)
 
-        # Для визуализации захваченной области с контурами
-        cv2.imshow("Captured Region", frame)
-        cv2.waitKey(1)  # Обновляем изображение
-        time.sleep(0.05)
+        # вывод tkinter
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(rgb_frame)
+        image_tk = ImageTk.PhotoImage(image)
+        label.imgtk = image_tk  # Сохраняем ссылку на изображение
+        label.config(image=image_tk)
+
+        root.update()
+
         if clicking_enabled:
+            time.sleep(0.04)
             click_element_contours(contours)
-    cv2.destroyAllWindows()
+        else:
+            time.sleep(0.2)
+
+
     print("Capture and processing thread terminated")
+    stop_all()
 
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
+def stop_all():
+    global program_running
+    program_running = False
+    root.quit()  # Завершаем работу окна tkinter
+    print("Program fully stopped")
 
-capture_thread = threading.Thread(target=capture_and_process)
-capture_thread.start()
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
 
-try:
-    listener.join()
-    capture_thread.join()
-except KeyboardInterrupt:
-    cv2.destroyAllWindows()
-    print("Program terminated")
+    capture_thread = threading.Thread(target=capture_and_process)
+    capture_thread.start()
+
+    root = tk.Tk()
+    root.resizable(False, False)
+    root.title("Blum_Clicker")
+    root.geometry("370x660+1400+300")
+
+    label = tk.Label(root)
+    label.pack(side='bottom')
+
+    label_with_bg = tk.Label(root,
+                            text="Нажмите правый Ctrl, чтобы начать или закончить",
+                            bg="blue",
+                            fg="white",
+                            font=("Arial", 18),
+                            wraplength=320)
+    button_status = tk.Button(root,
+                            text="OFF",
+                            bg="red",
+                            fg="white",
+                            font=("Arial", 20),
+                            wraplength=220,
+                            command= lambda: on_press(keyboard.Key.ctrl_r))
+                             
+    label_status = tk.Label(root,
+                            text="OFF",
+                            bg="red",
+                            fg="white",
+                            font=("Arial", 20),
+                            wraplength=220)
+    label_backspace = tk.Label(root,
+                               text="Выйти = Backspace",
+                               bg="black",
+                               fg="white",
+                               font=("Arial", 13),)
+    
+    label_backspace.pack(side='top',fill="x")
+    label_with_bg.pack(side='left',fill='both')
+    # label_status.pack(side='right',fill='both')
+    button_status.pack(side='right',fill='both')
+
+    
+
+    # Добавляем обработчик закрытия окна
+    root.protocol("WM_DELETE_WINDOW", stop_all)
+
+    root.mainloop()
